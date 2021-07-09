7bbfda9ed7652904a9bb0a434698aba530e6fcfb
Tiana Dueck <tianadueck@pm.me>
2021-07-08 22:30:36 -0400

    chatpag

---
+++ src/views/main/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <MainLayout>
+
+    <Chat />
+
+  </MainLayout>
+</template>
+
+
+<script>
+import MainLayout from '../../layouts/MainLayout'
+
+
+export default {
+  name: 'Chat',
+  components: {
+    MainLayout,
+    Chat
+  }
+  
+};
+</script>
