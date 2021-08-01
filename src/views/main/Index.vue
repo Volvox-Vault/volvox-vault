9ac642cefdb76e0663e3f022439884673df45069
Tiana Dueck <tianadueck@pm.me>
2021-07-31 22:17:15 -0400

    alchemypng

--- src/views/main/Index.vue
+++ src/views/main/Index.vue
@@ -1,6 +1,61 @@
 <template>
   <MainLayout>
 
+
+    <head>
+      <title>
+        Volvox Vault - An essay by Jake Fee, featured in Metamorphosis.
+      </title>
+
+      <link rel="shortcut icon" type="image/png" href="https://cdn.glitch.com/31c7994a-ff45-4592-b835-14262460c504%2Fjake.png?v=1627754078271" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <meta
+        name="description"
+        content="An essay by Jake Fee, featured in Metamorphosis."
+      />
+      <meta name="author" content="Jake Fee" />
+
+      <meta
+        property="og:title"
+        content="Volvox Vault - An essay by Jake Fee, featured in Metamorphosis."
+      />
+      <meta
+        property="og:description"
+        content="An essay by Jake Fee, featured in Metamorphosis."
+      />
+      <meta
+        property="og:image"
+        content="https://cdn.glitch.com/31c7994a-ff45-4592-b835-14262460c504%2Fjake.png?v=1627754078271"
+      />
+      <meta
+        property="og:url"
+        content="http://volvoxvault.com/articles/2021/homeCooking/"
+      />
+
+      <meta
+        property="twitter:title"
+        content="Volvox Vault - Better Living Through Alchemy"
+      />
+      <meta
+        property="twitter:description"
+        content="An essay by Jake Fee, featured in Metamorphosis."
+      />
+      <meta
+        property="twitter:image"
+        content="https://cdn.glitch.com/31c7994a-ff45-4592-b835-14262460c504%2Fjake.png?v=1627754078271"
+      />
+      <meta
+        property="twitter:url"
+        content="http://volvoxvault.com/articles/2021/homeCooking/"
+      />
+    </head>
+
+
+
+
+
     <script data-goatcounter="https://volvoxvault.goatcounter.com/count"
         async src="//gc.zgo.at/count.js"></script>
     
