af40691b0296e90342166670d276a4fe513483d8
Tiana Dueck <tianadueck@pm.me>
2021-09-03 21:00:28 -0400

    vol5

--- src/views/main/Contribute.vue
+++ src/views/main/Contribute.vue
@@ -18,42 +18,40 @@
           <li>
             <b>Managing Editor</b><br />
             We are looking for a managing editor with a passion for community
-            and creativity to help us source new articles and projects through
-            reaching out to people who are doing creative things that we ought
-            to cover and give a spotlight to. Additionally, the managing editor
+            and creativity to help us conceptualize new articles and projects through
+            reaching out to people who are doing cool creative things. They would be an active community member, looking for and producing creative pieces to feature in Volvox Vault. Additionally, the managing editor
             is someone who ponders the next steps for Volvox Vault as a
             whole—considering ideas for creative, financial, and community
             growth.
           </li>
           <li>
             <b>Editor</b><br />
-            We are looking for editors with a passion for
-            community and creativity to help us source new articles and projects
-            through reaching out to people who are doing creative things that we
-            ought to cover and give a spotlight to.
+            We are looking for editors with a passion for community
+            and creativity to help us conceptualize new articles and projects through
+            reaching out to people who are doing cool creative things. They would be an active community member, looking for and producing creative pieces to feature in Volvox Vault. 
           </li>
           <li>
             <b>Writer</b><br />
             We are looking for writers that we can go to with
-            ideas for articles, projects, and such who are up for the challenge
-            of making ideas into realities.
+            leads for articles, projects, and such who are up for the challenge
+            of making our ideas into realities.
           </li>
           <li>
             <b>Community Creative</b><br />
             We are looking for community creatives
             with unique skills like design, programming, photography,
-            videography, illustration, and more, who want to practice their
-            skills within our community for assets which would be featured on
+            videography, illustration, and more, who want to practice within our community. 
+            Creating assets which would be featured on
             our website, articles, projects, publications, and merch.
           </li>
           <li>
             <b>Community Coordinator</b><br />
             We are looking for community
-            coordinators who love working with new people, to help us with
+            coordinators who love working with new people to help us with
             producing projects, and managing roles and deadlines.
           </li>
           <li>
-            <b>Grant Writer</b><br />
+            <b>Grant Writer / Financial Manager</b><br />
             We are looking for someone who can help us
             grow into a community that can pay its contributors, and has the
             means to sell our publications and such at more affordable prices!
