e0f16696c360bf59599fccf230d7691bc299767f
Tiana Dueck <tianadueck@pm.me>
2021-12-24 22:02:43 -0500

    alex pond 2

--- src/views/main/Pond.vue
+++ src/views/main/Pond.vue
@@ -154,12 +154,27 @@ hr {
   background-color: grey;
 }
 
+.videoWrapper {
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  height: 0;
+}
+
+.videoWrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+/*
 iframe {
   width: 560px;
   height: 315px;
   margin-bottom: 0px;
   padding-bottom: 0px;
-}
+}*/
 
 @media (max-width: 1990px) {
   .pondpic {
