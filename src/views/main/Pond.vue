41274764d006393db96a29170f96adb8b942e511
Tiana Dueck <tianadueck@pm.me>
2021-12-24 22:43:35 -0500

    alex pond 2

--- src/views/main/Pond.vue
+++ src/views/main/Pond.vue
@@ -50,8 +50,8 @@
           the pond is a place for us to hang out. every 48 hours, the pond refreshes. <!--we have <a style="color: grey; text-decoration: underline" href="https://youtube.com/playlist?list=PLHTW864KflNnohBABQv0QCtorvWeHQOdV" target="_blank">picnics</a>.-->
         </p>
 
-        <video controls>
-        <source src="../../assets/pond/alexnunesbedholidaypicnic_2.mp4" type="video/mp4">
+        <video autoplay controls>
+        <source src="https://cdn.glitch.me/31c7994a-ff45-4592-b835-14262460c504/alexnunesbedholidaypicnic_2.mp4?v=1640402928018" type="video/mp4">
         Your browser does not support the video tag.
         </video> 
 
