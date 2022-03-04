975491f2b6b112defbb611491f49d86af3126f4f
Tiana Dueck <tianadueck@pm.me>
2022-03-03 22:10:17 -0500

    iframe size heart

--- src/views/archive/issues/Heart/Heart.vue
+++ src/views/archive/issues/Heart/Heart.vue
@@ -38,7 +38,7 @@
 <hr>
 
       <h2 id="heartathome">heart @ home</h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/N02aUFm4evM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="1080" height="1920" src="https://www.youtube.com/embed/N02aUFm4evM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </body>
   </div>
 </template>
