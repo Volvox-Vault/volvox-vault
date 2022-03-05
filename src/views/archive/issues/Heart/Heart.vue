6d124dff11c207b8f0881a04573ddf452fcae475
Tiana Dueck <tianadueck@pm.me>
2022-03-05 12:34:22 -0500

    buy button heart

--- src/views/archive/issues/Heart/Heart.vue
+++ src/views/archive/issues/Heart/Heart.vue
@@ -120,7 +120,7 @@
 
       
 <footer style="position: fixed; bottom: 0; width: 100%;
-padding-bottom: 0.5em; font-size: 20pt;;">
+padding-bottom: 0.5em; font-size: 20pt;">
   <p>
     <a style="padding: 10px; background: PaleVioletRed; border: 1px dotted ivory;" href="https://square.link/u/PjrmPfus"BUY HEART</a> 
   </p>
