63d6b4589b7f82ef6ba229bf37dd2c906c71f2b3
Tiana Dueck <tianadueck@pm.me>
2022-07-12 22:02:28 -0400

    columns

--- src/views/main/About.vue
+++ src/views/main/About.vue
@@ -5,8 +5,11 @@
       <div class="block-About">
         <p class="block-About-header">ABOUT</p>
         <p class="block-About-link">
-          <img style="width: 25%" src="https://cdn.glitch.me/31c7994a-ff45-4592-b835-14262460c504%2FAsset%201.png?v=1637158464886" />
-          Volvox is a thoughtful creative community.
+          <img
+            style="width: 25%"
+            src="https://cdn.glitch.me/31c7994a-ff45-4592-b835-14262460c504%2FAsset%201.png?v=1637158464886"
+          />
+          volvox.observer is a thoughtful creative community.
 
           <br />
           A
@@ -15,46 +18,118 @@
           >
           is a type of algae colony made up of smaller volvox colonies. It's a
           similar matrix sensation to when you point your front camera into a
-          mirror. The volvox would not exist without its colonies... likewise, as
-          creatives and human beings we thrive with support from our colonies /
-          communities. Volvox Observer is a special hub to mindfully come together and
-          archive our creations and connections.
+          mirror. The volvox would not exist without its colonies... likewise,
+          we thrive with support from our colonies/communities. volvox.observer
+          is a special hub to gently come together and archive our creations,
+          thoughts, and connections.
+          <br />
+          If you would like, you can support us on
+          <a href="https://www.patreon.com/volvoxvault">patreon</a> or
+          <router-link to="support">some other way</router-link>.
         </p>
 
-        <p style="text-align: center; max-width: 450px; margin: 40px auto;padding: 50px; background: #fbfdf7; border-radius: 100px; color: #212121;">
-      <b>CONTRIBUTE</b><br><br>
-      
-      We are seeking 1-2 contributers per month.<br>
-      As a small community effort, we offer...<br> 
-      $50 for pieces over 1000 words<br> 
-      $30 for pieces between 500-999 words<br> 
-      $20 for mini pieces between 300-500 words<br> 
-      Email us about pricing for visual focused pieces.
-      <br><br>
-  
-      Got an idea? Send your pitch to hello@volvoxvault.com (email change coming soon)
-      <br><br>
-      Want to support us so we can pay our contributors?
-      <br>
-<a href="https://patreon.com/volvoxvault">Join our Patreon</a>
-      or email us about buying ad space.
-    </p>
+        <div
+          style="
+            text-align: center;
+            max-width: 650px;
+            margin: 40px auto;
+            padding: 50px;
+            background: #fbfdf7;
+            border-radius: 100px;
+            color: #212121;
+          "
+        >
+          <h2>CONTRIBUTE</h2>
+          <p>
+            We are seeking 1-2 contributers per month.<br />
+            As a small community effort, we offer...<br />
+            $50 for pieces over 1000 words<br />
+            $30 for pieces between 500-999 words<br />
+            $20 for mini pieces between 300-499 words<br />
+            Email us about pricing for visual focused pieces.
+            <br />
+          </p>
+          <h2>COLUMNS</h2>
+          <br />
+          <div
+            style="
+              padding: 0px;
+              border: 1px dotted #212121;
+              border-radius: 100px;
+            "
+          >
+            <p style="padding: 20px">
+              <strong>DEAR DIARY</strong>: PERSONAL REALIZATIONS <br />
+              Got a thought stuck on your mind? Hash it out on volvox.observer.
+              Unravel a topic that's special to you with your best research and
+              opinions... the more obscure the better.
+            </p>
+            <hr />
+            <p style="padding: 20px">
+              <strong>NOTEBOOK</strong>: LESSONS + TUTORIALS <br />
+              Are you good at something, or getting there? Share your knowledge
+              on volvox.observer with an educational piece that could inspire
+              readers to delve into something new or old.
+            </p>
+            <hr />
+            <p style="padding: 20px">
+              <strong>FEELINGS</strong>: GETTING EMOTIONAL <br />
+              Explore a feeling that stands out to you. Whether the feeling or
+              something that brings up a certain feeling is at the centre of
+              your piece, get emotional! And ask your therapist for a quote.
+            </p>
+            <hr />
+            <p style="padding: 20px">
+              <strong>PERSONAL GOSSIP</strong>: WHAT'S NEW?<br />
+              Share about a new creative endeavour, an iconic recipe, a sewing
+              pattern, a short film, etc. Embody the likes of Carrie Bradshaw,
+              Gossip Girl — even Jonah Weiner (of blackbird spyplane), or Rachel
+              Tashjian (of opulent tips). Just don't forget to bring the
+              (friendly) drama!
+            </p>
+            <hr />
+            <p style="padding: 20px">
+              <strong>KIND WORDS</strong>: EXCHANGING KIND WORDS WITH MAD<br />
+              An unpaid letter style column with Mad as our host.
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLScaMRoojXD19ClmhFsylL6NPGA6jKA98DxdhSafjnZnNCURQA/viewform"
+                >Contribute here.</a
+              >
+            </p>
+          </div>
+          <br />
 
-    <p class="block-About-link" style="text-align: center;">
-       If you would like, you can support us on <a href="https://www.patreon.com/volvoxvault">patreon</a> or <router-link to="support">some other way</router-link>.
-          <br /><br />
+          <p>
+            Got an idea? Send your pitch to hello@volvoxvault.com <br />
+            (email change coming soon)
+            <br />
+          </p>
+
+          <p>
+            Want to support us so we can pay our contributors?
+            <br />
+            <a href="https://patreon.com/volvoxvault">Join our Patreon</a>
+            or email us about buying ad space.
+          </p>
+        </div>
+
+        <h2 style="text-align: center">MASTHEAD</h2>
+        <p class="block-About-link" style="text-align: center">
           Community Manager —
           <a href="https://tiana.computer/" target="_blank">Tiana Dueck</a
           ><br />
           Website Team — Tiana Dueck &#38;
           <a href="https://ari.computer/" target="_blank">Ari Lotter</a><br />
-          Horoscopes —
-          <a href="https://volvoxvault.com/horoscopes">Julie MoontidePoet</a>
+          Volunteer Editors —
+          <a href="https://henryland.net/homepage">Henry</a>,
+          <a href="https://polapola.format.com/">Pola</a>, Aditi, George,
+          <a href="https://polapola.format.com/">Seán</a>,
+          <a href="https://chris.e-worm.club/portfolio.gmi">Chris</a>
           <br />
           <router-link to="/contribute">Join our team.</router-link><br /><br />
 
           hello@volvoxvault.com
-    </p>
+        </p>
       </div>
     </div>
 
@@ -125,7 +200,7 @@ export default {
   float: right;
   max-width: 250px;
   padding-bottom: 15px;
-  padding-left: 15px;
+  padding-left: 25px;
 }
 
 .block-About-link a,
@@ -135,7 +210,8 @@ export default {
 }
 
 .block-About-link a:hover,
-.block-shop a:hover {
+.block-shop a:hover,
+a:hover {
   text-decoration: underline;
   cursor: pointer;
 }
