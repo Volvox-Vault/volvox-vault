82227e916607f53f922f6a7e3db42221c8d7fbea
Tiana Dueck <tianadueck@pm.me>
2022-10-21 11:00:23 +0200

    hi3

---
+++ src/views/archive/2022/11/belongingandcreativity.vue
@@ -0,0 +1,344 @@
+<template>
+  <body>
+
+<main>
+        
+        <h1 class="title">Belonging + Creativity</h1>
+      <br>  
+      <p class="by" style="font-size: 14pt;">By Sophie Spencer for <router-link to="/">Volvox Observer</router-link></p>     
+      <br>
+      <hr />
+        <br />
+        
+        <p>
+          My body collects tattoos like pieces of art hung on gallery walls...
+          
+          <br /><br />
+          
+          I have fourteen and counting (my latest joining the party just
+          yesterday afternoon). I wish to be covered in patchwork, with slivers
+          of arm and leg peeking through. In my art-covered body, I am home. I
+          belong. With each piece of inspired art adorning my skin, my
+          reflection belongs more to me—to the truth of who I am when I close my
+          eyes within a vast silence.
+
+          <br /><br />
+          
+          Sitting in a sun-lit tattoo studio in Seattle, Washington, my eyes
+          wander—I’m trying to distract myself from the pain of a fresh tattoo
+          being drawn on my left thigh. I notice a framed work of art, a
+          black-Sharpie-on-white-paper sketched mish-mash collage. My eyes trace
+          from an octopus, to a tiger, to a cowboy on a horse, to a bus full of
+          people, to a woman in an impressive hat, and on and on through words
+          and doodles… It’s like a sketchbook, or a patchwork tattooed-body.
+          Perhaps they’re oneinthesame.
+          
+          <br /><br />
+           
+          The work has no discernable theme beyond the imagination of the
+          artist. Simply woven together by oh, this would go here! I like that
+          there. These words sound nice. I like the way that fits. Taking it in,
+          I wish the artist was here to guide me through the connections—from
+          octopus, to horse-riding cowboy, to a woman in a big hat.
+
+          <br /><br />
+          
+          Nevertheless, sitting here, I will never know. I was not there when
+          this art was made, and I will never be walked through why all these
+          things fit together. Still, in that unknown I feel a blossoming of
+          peace. These random elements brought together in a puzzle of chaos
+          feel rather complete together. Maybe they aren’t here for me to make
+          sense of, but to curiously admire. To be transported by. This
+          illogical collection of images warmly invites me into its disarray.
+
+          <br /><br />
+          
+          All the while, my body is becoming its own patchwork sketchbook of my
+          human experience. Not tidied up or masked over, not restricted or
+          deluded. Rather, brought together in its messiness. Made whole by
+          tying my random lawless pieces into braids of compassion, curiosity,
+          and acceptance.
+          <br /><br />
+          My eyes, still digging through the large print, flutter from piece to
+          piece. In the bottom left corner it reads:
+          <br /><br /><em>
+            Full of energy and peaceful at the same time. Like the ocean, like
+            bluegrass banjo. <br />
+            Like the wind. Like the bicycle. Like the sun.
+          </em>
+          <br /><br />
+          That is the feeling of <em>Belonging</em>.
+
+          <br /><br />
+          
+          <em>Belonging</em> is a space of peace where life effortlessly flows.
+          A soothing sort of <em>all is welcome and life is beautiful.</em> And
+          belonging catapults me into creativity. 
+          <br /><br />
+          In belonging, I am free to create myself endlessly, because it is
+          where truth, life, and creation are all combined. Creation flows like
+          sunlight through transparent leaves, dotting the forest below. It
+          spreads thin and easy like butter on warmed toast. It is water, and I
+          am the name of the river.
+          <br /><br />
+          In my creative practices, be them words, fiber, or paint, free
+          creation is something I’ve struggled to feel at home with. Outside of
+          belonging, my creativity becomes sticky—forced and judged harshly. I
+          spend more time critiquing and planning my art than living it—or
+          allowing it to live through me.
+          
+          <br /><br />
+          
+          I feel whole when I am dancing. When I glance at my art-covered body
+          in a mirror, I feel my heart moving me from within. I see my arms and
+          my legs moving from something of a deeper truth than the name I was
+          given, and I remember what it feels like to belong. Creating there
+          feels easy, in a form that is there one minute and gone the next, in
+          its beautiful ephemerality.
+          <br /><br /><em>
+            Sunlight on water. <br />
+            The smell of rain on grass. <br />
+            A loved one’s head landing on your shoulder.
+          </em>
+
+          <br /><br />
+          
+          It feels easier to me to create ephemerally. To seek bits of beauty
+          meant to live and die. Tattoos even, although they are sunken into my
+          lifetime companion, are ephemeral in a way. They will leave with my
+          body, they will decay with time. They do not hold onto their mortality
+          with clenched fists. Rather they adorn, dance, and crackle up toward
+          the sky like laughter on the night, and they leave with me.
+
+          <br /><br />
+          
+          Maybe, as creativity is a language in and of itself, I have not yet
+          found my mother tongue. Or maybe all creativity needs is an open
+          container, and doubt is a foggy film of judgment obscuring my
+          belonging. Maybe whatever comes through is my fluency—if I have ears
+          to listen, a mouth to speak, and a seat within belonging to guide it.
+
+          <br /><br />
+          
+          Creativity calls for a loving and accepting mindspace—for an inviting
+          and inspiring environment—for support from those around you. It calls
+          for belonging on all fronts, beginning within. My art cannot be fully
+          true, to the deepest parts of me, before I belong to myself. I belong
+          to myself when I see all parts of me as truth. When I find solace
+          within my flesh and bone home that I have built and decorated. In my
+          messiness, in my seemingly random puzzle of multifaceted humanity, in
+          all of my fragments.
+
+          <br /><br />
+          
+          My personal internal belonging is this (abridged):
+          <br /><br />
+          My daily fluctuation in energy levels, in desires, in moods and
+          mindspaces. <br />
+          My swiftly-moving inspiration for differing mediums. My shelves, full
+          of craft supplies. <br />
+          My words as they flow like jumbled thread at first, which I untangle
+          and guide to their seamlessness.
+          <br /><br />
+          <em>And.</em>
+          <br /><br />
+          The doubt that I have learned. <br />
+          The fear as it teaches me things about myself. <br />
+          The pieces of myself that feel too big and scary to look at, much less
+          to speak.
+
+          <br /><br />
+          
+          My journey through creativity winds from home to places ever outward.
+          It has led to places I hadn’t yet accepted, so I could grow love into
+          them and expand my definition of what is creatively acceptable.
+          <br /><br />
+          Belonging is a state of being which ushers in delicious, potent life.
+          It is the bed of creativity, one that I value in all parts of my life:
+          my art, body, relationships, career, solitude… Not as a destination,
+          but as an invitation to deeper love for myself and the world.
+
+          <br /><br />
+          
+          In that tattoo studio, this feeling met me while looking at someone’s
+          art, while creating art on my patchwork skin.
+          <br /><br />
+          And now, the trees rustle in today’s afternoon wind, as do I in my
+          patchwork tattoo skin, and my fingers typing this here. I welcome
+          whatever may flow through—and as I am tying together these pieces of
+          my messiness with curiosity and compassion, <em>I belong.</em>
+        </p>
+        <br />
+
+    </main>
+    
+
+    <div style="position: fixed; bottom: 0; width: 100%">
+      
+      <div
+        style="
+          width: 100%;
+          max-height: 15vh;
+          overflow: scroll;
+          background: white;
+          border-top: 2px dotted #6f4e72;
+          border-bottom: 2px dotted #6f4e72;
+        "
+      >
+        <p id="para">Click the images to discover...</p>
+      </div>
+      
+      <div class="TATrow">
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(1)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/art%20sq.jpg?v=1665954561805"
+            alt="Snow"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(2)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/68221226406__7BDBA268-1A78-4865-B3FF-56446D7445D5.jpeg?v=1664644236095"
+            alt="Snow"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(3)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/IMG_0277.jpeg?v=1664644236212"
+            alt="Forest"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(4)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/67618287574__530A8D98-66DB-49C2-AD77-BFCDD00B2D18.jpeg?v=1664644236296"
+            alt="Mountains"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(5)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/IMG_0279.jpeg?v=1664644236384"
+            alt="Mountains"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(6)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/IMG_0163.jpeg?v=1664644236710"
+            alt="Forest"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(7)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/IMG_6839.jpeg?v=1664644236569"
+            style="width: 100%"
+          />
+        </div>
+        <div class="TATcolumn">
+          <img
+            onclick="changeText(8)"
+            src="https://cdn.glitch.global/25997b0f-8076-48ed-9905-f08bf8646cf5/IMG_0280.jpeg?v=1664644236523"
+            alt="Mountains"
+            style="width: 100%"
+          />
+        </div>
+      </div>
+    </div>
+
+  </body>
+</template>
+
+
+<script>
+
+export default {
+  name: "belongingandcreativity",
+  components: {
+  },
+  metaInfo: {
+    // title will be injected into parent titleTemplate
+    title: "Belonging + Creativity",
+  },
+};
+
+</script>
+
+<style scoped>
+@font-face {
+        font-family: hershey;
+        src: url(https://cdn.glitch.global/fdbdb418-0e5c-49d6-bb3a-6ff7dff79fc5/Hershey-Noailles-Futura-Simplex-Light.otf?v=1643946358610);
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  max-width: 2000px;
+  height: 100vh;
+  overflow: none;
+  background: #eee7ee;
+  color: #222222;
+  font-family: hershey, arial, sans-serif;
+}
+
+main {
+  padding: 30px;
+  font-weight: normal;
+  height: 65vh;
+    overflow: scroll;
+
+}
+
+img {
+  width: 100%;
+  display: block;
+}
+
+.by {
+  color: #6f4e72;
+}
+
+p {
+  line-height: 1.6;
+  font-size: 1.5em;
+  margin: 0px;
+}
+
+hr {
+  border: none;
+  border-top: 2px dotted #6f4e72;
+}
+
+.title {
+  color: #6f4e72;
+  font-weight: normal;
+  padding: 0px;
+  margin: 0px;
+}
+
+#para {
+  font-size: 14pt;
+  padding: 8px;
+}
+
+.TATcolumn {
+  float: left;
+  width: 12.5%;
+}
+
+/* Clearfix (clear floats) */
+.TATrow::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
