f9bd05e818aa3507aa4fbb554d83a666b9e9cfb4
Tiana Dueck <tianadueck@pm.me>
2022-12-31 14:44:49 +0100

    myles7

--- src/views/archive/2022/12/myleswheeler.vue
+++ src/views/archive/2022/12/myleswheeler.vue
@@ -30,6 +30,7 @@
                     padding: 20px;
                     text-align: center;
                     margin-top: 20px;
+                    margin-bottom: 20px;
                   "
                 >
                   <em
@@ -39,7 +40,7 @@
                   >
                 </p>
 
-<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0MflRtDYu4UBWfcIb9WUvb?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<iframe style="border-radius:30px" src="https://open.spotify.com/embed/episode/0MflRtDYu4UBWfcIb9WUvb?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
                 <p
                   style="
