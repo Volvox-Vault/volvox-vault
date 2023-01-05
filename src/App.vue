711facdd427759c394da60e991b012df5a447141
Tiana Dueck <tianadueck@pm.me>
2023-01-05 21:47:21 +0100

    imageMeta

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
       // if no subcomponents specify a metaInfo.title, this title will be used
       title: 'Volvox Observer',
       // all titles will be injected into this template
-      titleTemplate: '%s | Volvox Observer'
+      titleTemplate: '%s | Volvox Observer',
+      image: 'https://cdn.glitch.global/31c7994a-ff45-4592-b835-14262460c504/usr.png?v=1651526023814'
     }
   }
 </script>
