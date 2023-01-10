791925111504e59e8b45971e4188691c785b5166
Tiana Dueck <tianadueck@pm.me>
2023-01-10 22:49:08 +0100

    true13

--- src/views/archive/2023/1/trueself.vue
+++ src/views/archive/2023/1/trueself.vue
@@ -32,6 +32,15 @@
         </ul>
 
         </p>
+        <br>
+        <hr>
+        <br>
+        <p>
+        The festival will be published online on volvox.observer. Each film will have its own webpage that showcases the film as well as special features (so document your production process!), like photos, Q&As, a director's statement, and more. 
+<br><br>
+We are also planning to host an in person festival in Toronto, and hopefully somewhere in the Netherlands — details TBD. We will also likely host an online live premiere of the fest. Ticket sale profits will be distributed to contributing artists.
+
+        </p>
     </body>
 
 </template>
