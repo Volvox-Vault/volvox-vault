26d3dc4642a235216efd720aa47fcc02e2d20df9
Tiana Dueck <tianadueck@pm.me>
2023-04-02 17:07:46 +0200

    router true

--- src/views/archive/2023/1/trueselftemplate.vue
+++ src/views/archive/2023/1/trueselftemplate.vue
@@ -1,6 +1,6 @@
 <template>
     <body>
-        <img src="https://cdn.glitch.global/31c7994a-ff45-4592-b835-14262460c504/ts.png?v=1673389523276" class="logo" />
+        <router-link to="trueself"><img src="https://cdn.glitch.global/31c7994a-ff45-4592-b835-14262460c504/ts.png?v=1673389523276" class="logo" /></router-link>
     
         <h1>your film's title</h1>
         <br>
