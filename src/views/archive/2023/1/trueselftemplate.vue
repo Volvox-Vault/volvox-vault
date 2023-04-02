9173aad3ad1bc5b59bb95aa346f87a57b5764ead
Tiana Dueck <tianadueck@pm.me>
2023-04-02 17:06:28 +0200

    temp chat 300

--- src/views/archive/2023/1/trueselftemplate.vue
+++ src/views/archive/2023/1/trueselftemplate.vue
@@ -37,7 +37,7 @@
 
                 <br>
 
-        <script type="application/javascript" id="cid0020000341434605351" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 400px;">{"handle":"trueselfff","arch":"js","styles":{"a":"f3f9e6","b":100,"c":"664c71","d":"664c71","k":"f3f9e6","l":"f3f9e6","m":"f3f9e6","p":"10","q":"f3f9e6","r":100,"ab":false,"usricon":0,"surl":0,"allowpm":0,"cnrs":"1","fwtickm":1}}</script>
+        <script type="application/javascript" id="cid0020000341434605351" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 300px;">{"handle":"trueselfff","arch":"js","styles":{"a":"f3f9e6","b":100,"c":"664c71","d":"664c71","k":"f3f9e6","l":"f3f9e6","m":"f3f9e6","p":"10","q":"f3f9e6","r":100,"ab":false,"usricon":0,"surl":0,"allowpm":0,"cnrs":"1","fwtickm":1}}</script>
             
     
             </div>
