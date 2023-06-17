91c5b5fa32fc9e571e83513ec014bd9fb12213de
Tiana Dueck <tianadueck@pm.me>
2023-06-17 14:28:57 +0200

    clean

--- src/views/archive/2023/2/cleaning.vue
+++ src/views/archive/2023/2/cleaning.vue
@@ -144,67 +144,13 @@
 
 <script>
 export default {
+    import 'cleaning.js';
+
     name: "cleaningdiary",
     components: {},
     metaInfo: {
         // title will be injected into parent titleTemplate
         title: "cleaning diary",
-    },
-    created: function() {
-      function myFunction1() {
-        document.getElementById("img1").style.display = "none";
-      }
-      function myFunction2() {
-        document.getElementById("img2").style.display = "none";
-      }
-      function myFunction3() {
-        document.getElementById("img3").style.display = "none";
-      }
-      function myFunction4() {
-        document.getElementById("img4").style.display = "none";
-      }
-      function myFunction5() {
-        document.getElementById("img5").style.display = "none";
-      }
-      function myFunction6() {
-        document.getElementById("img6").style.display = "none";
-      }
-      function myFunction7() {
-        document.getElementById("img7").style.display = "none";
-      }
-      function myFunction8() {
-        document.getElementById("img8").style.display = "none";
-      }
-        function myFunction9() {
-        document.getElementById("img9").style.display = "none";
-      }
-      function myFunction10() {
-        document.getElementById("img10").style.display = "none";
-      }
-      function myFunction11() {
-        document.getElementById("img11").style.display = "none";
-      }
-      function myFunction12() {
-        document.getElementById("img12").style.display = "none";
-      }
-      function myFunction13() {
-        document.getElementById("img13").style.display = "none";
-      }
-      function myFunction14() {
-        document.getElementById("img14").style.display = "none";
-      }
-      function myFunction15() {
-        document.getElementById("img15").style.display = "none";
-      }
-      function myFunction16() {
-        document.getElementById("img16").style.display = "none";
-      }
-      function myFunction17() {
-        document.getElementById("img17").style.display = "none";
-      }
-      function myFunction18() {
-        document.getElementById("img18").style.display = "none";
-      }
     },
 };
 </script>
