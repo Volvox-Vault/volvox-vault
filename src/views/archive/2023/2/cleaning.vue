9c4be9c5cb1065b4fc94c46862b523e19cc66917
Tiana Dueck <tianadueck@pm.me>
2023-06-17 14:21:36 +0200

    ok

--- src/views/archive/2023/2/cleaning.vue
+++ src/views/archive/2023/2/cleaning.vue
@@ -138,63 +138,6 @@
       src="https://cdn.glitch.global/f78862a1-d039-4c43-af38-52de88c188df/athen-sketches-06.png?v=1684588280762"
     />
 
-    <script>
-      function myFunction1() {
-        document.getElementById("img1").style.display = "none";
-      }
-      function myFunction2() {
-        document.getElementById("img2").style.display = "none";
-      }
-      function myFunction3() {
-        document.getElementById("img3").style.display = "none";
-      }
-      function myFunction4() {
-        document.getElementById("img4").style.display = "none";
-      }
-      function myFunction5() {
-        document.getElementById("img5").style.display = "none";
-      }
-      function myFunction6() {
-        document.getElementById("img6").style.display = "none";
-      }
-      function myFunction7() {
-        document.getElementById("img7").style.display = "none";
-      }
-      function myFunction8() {
-        document.getElementById("img8").style.display = "none";
-      }
-        function myFunction9() {
-        document.getElementById("img9").style.display = "none";
-      }
-      function myFunction10() {
-        document.getElementById("img10").style.display = "none";
-      }
-      function myFunction11() {
-        document.getElementById("img11").style.display = "none";
-      }
-      function myFunction12() {
-        document.getElementById("img12").style.display = "none";
-      }
-      function myFunction13() {
-        document.getElementById("img13").style.display = "none";
-      }
-      function myFunction14() {
-        document.getElementById("img14").style.display = "none";
-      }
-      function myFunction15() {
-        document.getElementById("img15").style.display = "none";
-      }
-      function myFunction16() {
-        document.getElementById("img16").style.display = "none";
-      }
-      function myFunction17() {
-        document.getElementById("img17").style.display = "none";
-      }
-      function myFunction18() {
-        document.getElementById("img18").style.display = "none";
-      }
-    </script>
-
     </body>
 </template>
 
@@ -207,7 +150,7 @@ export default {
         // title will be injected into parent titleTemplate
         title: "cleaning diary",
     },
-    data: function () {
+    created: function() {
       function myFunction1() {
         document.getElementById("img1").style.display = "none";
       }
