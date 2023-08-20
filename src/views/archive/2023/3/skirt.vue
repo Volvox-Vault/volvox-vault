9389a973e4b0833410564d139b96a5062a07b6c0
Tiana Dueck <tianadueck@pm.me>
2023-08-20 03:34:32 +0200

    skrt

--- src/views/archive/2023/3/skirt.vue
+++ src/views/archive/2023/3/skirt.vue
@@ -161,6 +161,20 @@ img {
   height: 30px;
 }
 
+.container {
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+}
+.video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+}
+
 .bandList {
   display: block;
   margin-left: auto;
