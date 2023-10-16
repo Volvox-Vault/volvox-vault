41e733d58d031f0818ec0faf8f00df9f0eeacc93
Tiana Dueck <tianadueck@pm.me>
2023-10-16 00:42:31 -0400

    diary21

--- src/views/main/Diary.vue
+++ src/views/main/Diary.vue
@@ -21,7 +21,7 @@
                 <div class="form">
                     <form ref="diaryEntry" class="diaryForm" @submit.prevent="submitForm" @submit="submitFormReset">
     
-                        <p style="margin-top: 20px;">Dear diary,</p>
+                        <p style="margin-top: 15px;">Dear diary,</p>
                         <textarea id="volvox-diary" rows="10" cols="60" name="message" v-model="message" placeholder="your diary entry + public social media/contact info (optional)" required></textarea>
     
                         <p>Sincerly,</p>
