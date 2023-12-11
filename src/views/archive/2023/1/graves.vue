2658b956381b059f7bf3d034b0af2d0197d53cfd
Tiana Dueck <tianadueck@pm.me>
2023-12-11 15:34:36 -0500

    am

--- src/views/archive/2023/1/graves.vue
+++ src/views/archive/2023/1/graves.vue
@@ -6,14 +6,12 @@
         class="logo"
     /></router-link>
 
-    <h1>MATADOR</h1>
+    <h1>I Am</h1>
     <br />
     <p style="font-family: hersheyFut">
       A film by
-      <a href="https://www.instagram.com/matias.vaisman/" target="_blank"
-        >Matias Teixeira Vaisman</a
-      >
-      (He/Him).
+      <a href="https://www.kailumgraves.com/" target="_blank">Kailum Graves</a>
+      (He/They).
     </p>
     <br />
 
@@ -29,7 +27,7 @@
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/zAUwJ4zgwF0"
+        src="https://www.youtube.com/embed/PYAHCRhrpxM?si=FRjzd6fcJoeRE3c0"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -44,15 +42,85 @@
         </h2>
         <br />
         <p>
-          Matador is a no-budget short made for a cinematography class at a
-          public Brazilian university. It is an experimental psychological
-          thriller on the angst of having a lonely job, that job being of a
-          contract killer. It's an exercise in conveying feelings purely through
-          the cinematic form, with as minimum written text as possible. In some
-          ways, an attempt to show that genre movies don't need a lot of money
-          to become real, just like our country's most legendary filmmaker
-          Glauber Rocha once said, "a camera in the hand and an idea in the
-          head".
+          A few things recently happened that were the culmination of a lifetime
+          of pain. To begin to understand, accept, and deal with everything—to
+          move on with my life—I wrote a timeline of my entire existence. It
+          contained my deepest Core (my soul, my Being, my consciousness, my
+          unconsciousness, my me-ness). It was my deepest self, my deepest Me,
+          full of my nethermost fears, anxieties, doubts, and insecurities. It
+          was my soul laid bare. A new beginning. My childhood and adolescent
+          years had long-lasting consequences on my perception of self. Yet, I
+          never spoke about my childhood, not even with the people closest to
+          me. I was embarrassed and ashamed. Instead, I expressed myself through
+          art (as cliché as this sentence sounds). My art—chronicling
+          billionaires, politicians, the porn industry, the invisible power
+          structures behind photography etc.—is me detesting people who believe
+          they have power. People who abuse other people for their own
+          self-gratification. Beings who crave power and crush other beings to
+          feel big. People who demand respect but do nothing to earn the respect
+          they command. Men who feel the urge to make women and children feel
+          vulnerable and small. This was my childhood. Yet, instead of facing
+          it, I ran, I hid, I felt alone, frightened, rejected, and unloved.
+          This project is an attempt to be me. It is me refusing to hide. It is
+          a self-reflective metaphysical question that I couldn’t answer until
+          recently—what does it feel like ‘to exist’? It is an attempt to yell
+          at the top of my fucking lungs that I exist—that ‘I am’. Nonetheless,
+          I never want to be defined by my perceived weakness—my abuse, my
+          post-traumatic stress, my angst, and my depression. My childhood made
+          me the person I am today, so I wouldn’t change the past, even if I
+          could. I exist.
+        </p>
+        <br />
+        <h3>KAILUM GRAVES</h3>
+        <p>
+          Kailum is critically obsessed with the Web and born-digital content.
+          He is particularly interested in image-rich technologies and the way
+          global media communication—a landscape controlled by a handful of
+          multidimensional oligopolistic corporate-run networks—can be sampled,
+          organised, and considered in new philosophical, sociological, and
+          political terms. Nonetheless, while these issues are political and
+          economic in nature, Kailum believes anti-capitalist art offers no real
+          alternative to the economic and ideological discourses of
+          multinational capitalism. In its place, he is interested in examining
+          the politics of the image and the construction of truth. To do this,
+          he uses the Internet, which has normalised the act of collecting and
+          compiling information, to preserve and curate found images and raw
+          material. The aim is to engage with the cultural space and aesthetics
+          of the Internet—and the vast amount of digital information it
+          contains—as a subject, material, and tool of artistic production.
+          <br /><br />
+          To do this, Kailum investigates different media and photographic
+          methods—from appropriation to pixel and data manipulation—to create
+          still and moving image projects that reflect the influence of
+          technology on the photographic medium and sit resolutely within the
+          context of future movements of contemporary photography. One of the
+          most convincing aspects of his approach is the way he urges the viewer
+          to evolve from a condition of mere spectatorship to a position where
+          they must actively reflect on the various roles of technology in our
+          media-driven age. However, rather than openly expressing
+          socio-political criticism, Kailum takes a gentler
+          ‘hint-at-the-direction’ path which invites the viewer to a process of
+          reflection. On the surface his work might seem easy to read, but on
+          closer inspection a viewer will find that he draws upon a myriad of
+          artistic, philosophical, scientific, theoretical, and
+          cross-disciplinary approaches to achieve his highly refined poetic
+          ends.
+          <br /><br />
+          Kailum’s research delicately exists in a space between photography,
+          Internet art, algorithmic art, and digital performance. He uses a
+          laptop the same way a photographer uses a camera to document, record,
+          and manipulate the world—which for Kailum is born-digital artefacts—to
+          explore the disappearance of clear boundaries between culture,
+          environment, and technology. He is particularly interested in the way
+          humans use technology to transform, manipulate, and transcend natural
+          environmental limits. There isn’t a one-way correlation; instead, a
+          culture develops a technology, such as agriculture, to change the
+          environment, and this technology, in turn, changes the culture and
+          society that created it. Kailum explores this closed loop by creating
+          large-scale still and moving images that play with the ubiquitous
+          dichotomies between physical and digital existence.
+          <br /><br />
+          Email Kailum: mrkailumgraves{@}gmail.com
         </p>
       </div>
 
@@ -61,77 +129,105 @@
           <span style="background: #f3f9e6; padding: 5px">CREDITS</span>
         </h2>
         <br />
-        <p>
-          Starring: José Miranda<br />
-          Written, Directed, Shot And Edited By: Matias Vaisman<br />
-          Assistant Directors: Pedro Perpétuo, Lipe Dal-cól & Isabelle De<br />
-          Freitas Producers: Kauane Castro, Pedro Perpétuo & Isabelle De
-          Freitas<br />
-          Art Direction: Beatriz Pinheiro Casting And Set Coordinator: Lipe
-          Dal-cól
-        </p>
+        <p>Director: Kailum Graves</p>
       </div>
 
       <div class="ts-box">
         <h2 style="color: #664c71">
-          <span style="background: #f3f9e6; padding: 5px">Q&A WITH MATIAS</span>
+          <span style="background: #f3f9e6; padding: 5px">Q&A WITH KAILUM</span>
         </h2>
         <br />
 
         <p>
-          <strong>Why were you inspired to make this specific film?</strong> ~ I
-          Wanted to make a film that was at once a genre movie focused on
-          process, a film about loneliness in the urban environment and an
-          experimental film, all of those being themes/aesthetics/qualities that
-          i deeply love in the pictures that i watch. So it was at the same time
-          an ambitious but humble attempt to use my insirations to make
-          something as independently as possible. <br /><br />
-          <strong>How does your true self shine through in your film?</strong> ~ By two means. The
-          first, by putting a lot of myself and my feelings at the time on the
-          character, and it being perceptible despite the obvious differences
-          (me not being a contract killer). The second, by making the film that
-          i wanted, having my clear vision and intent onscreen, which is
-          something that is always deeply personal and perhaps the clearest
-          expression of true self a filmmaker can achieve.
+          <strong>Why were you inspired to make this specific film?</strong>
+          <br /><br />
+          The inspiration for creating this specific film emerged from a series
+          of recent events that ultimately confronted a lifetime of emotional
+          pain. In an effort to grapple with and find acceptance for these
+          experiences, I embarked on a deeply introspective journey, chronicling
+          the entirety of my existence. This process revealed the core essence
+          of my being, encompassing my consciousness, fears, vulnerabilities,
+          and insecurities—an unfiltered representation of my soul.<br /><br />
+          <strong
+            >Did anyone specific become critical to the creation of your film? Why do they stand out as significant to your film?</strong
+          >
+          <br /><br />
+          Definitely, the support and connection I found with individuals who
+          shared similar backgrounds and histories were absolutely critical to
+          the creation of my film. These connections served as a powerful
+          reminder that I wasn't navigating this journey in isolation. Their
+          willingness to share their own experiences created a sense of
+          solidarity that encouraged me to confront my own past and find
+          strength in vulnerability. Furthermore, my artistic outlet was a
+          significant factor in shaping the film. It provided a medium through
+          which I could express emotions and experiences that felt nearly
+          impossible to articulate with words alone. This artistic process
+          helped me channel the complexity of my feelings into a tangible form,
+          giving me a renewed sense of agency and a way to communicate with
+          others on a profound level. Amidst the creation of the film, another
+          pivotal development was my decision to begin Eye Movement
+          Desensitization and Reprocessing (EMDR) therapy for my complex
+          post-traumatic stress disorder (C-PTSD). This therapeutic approach
+          became an integral part of my journey, contributing to my healing and
+          growth. It was during this time that I found myself increasingly
+          capable of transcending the limitations that my past had imposed on
+          me. The combination of artistic expression and therapy allowed me to
+          redefine my existence on my own terms, reshaping my narrative and
+          reaffirming my presence in a way that I never thought possible.
+          <br /><br />
+          <strong>Were there any challenges or doubts you faced in the process? How did you address them?</strong>
+          <br /><br />
+          Absolutely, there were a multitude of challenges and doubts that I encountered throughout this process. Confronting the lingering impact of my childhood and adolescence on my self-perception was deeply daunting. The silence surrounding my past, which I had maintained even with those closest to me, was born from a potent blend of embarrassment and shame. 
+<br><br>
+Undertaking this project marked an endeavour to shed the layers of concealment and to bravely embrace my authentic self. Yet, amidst this journey, I grappled with the concern of being pigeonholed by my perceived vulnerabilities—trauma, post-traumatic stress, anxiety, and depression. It was imperative to me that my narrative extended beyond these aspects, that they did not encapsulate the entirety of my identity. I recognised that my formative experiences, despite their difficulties, shaped me into the person I am today, a fact I came to accept and even embrace.
+        <br /><br />
+          <strong
+            >How does your true self shine through in your film?</strong
+          >
+          <br /><br />
+          In my film, my true self shines through in its unfiltered authenticity. It's a raw and unvarnished reflection of my journey, where I've confronted my past and embraced my vulnerabilities head-on. Through the film, I've harnessed the power of artistic expression to communicate emotions and experiences that words alone struggle to convey. This genuine portrayal of my struggles, triumphs, and self-discovery stands as a testament to my resilience and willingness to be vulnerable. By revealing both the pain and the growth, the film encapsulates my true self – a person who refuses to be defined solely by past hardships but instead thrives in the process of redefining existence on my own terms.
           <br><br>
-          <strong>What thoughts has making this film sparked that you may bring into your next project or into your daily life?</strong>
-Definetly that, for independent films, the less the better in terms of production. And that cheap, non-hd cameras can also produce images that look great!
+          Crafting this film frame-by-frame has sparked a multitude of thoughts that I'm truly excited to weave into my forthcoming projects. In fact, I took on the challenge of crafting a 5-hour video, meticulously constructing each frame, to vividly portray the intricacies of Life, the Universe, and Everything. Upon completing this endeavor, I found myself captivated by the process and intrigued to explore this approach on a grander scale in my future endeavors. 
+
+<a href="https://www.kailumgraves.com/cosmos/" target="_blank">The project can be viewed here.</a>
         </p>
-
-        <br />
-
-        <script
-          type="application/javascript"
-          id="cid0020000341434605351"
-          data-cfasync="false"
-          async
-          src="//st.chatango.com/js/gz/emb.js"
-          style="width: 100%; height: 300px"
-        >
-          {
-              "handle": "trueselfff",
-              "arch": "js",
-              "styles": {
-                  "a": "f3f9e6",
-                  "b": 100,
-                  "c": "664c71",
-                  "d": "664c71",
-                  "k": "f3f9e6",
-                  "l": "f3f9e6",
-                  "m": "f3f9e6",
-                  "p": "10",
-                  "q": "f3f9e6",
-                  "r": 100,
-                  "ab": false,
-                  "usricon": 0,
-                  "surl": 0,
-                  "allowpm": 0,
-                  "cnrs": "1",
-                  "fwtickm": 1
-              }
-          }
-        </script>
       </div>
+    </div>
+
+    <br />
+    
+    <div style="border: 1px dotted #212121; padding: 30px; height: 400px">
+      <script
+        type="application/javascript"
+        id="cid0020000363318126660"
+        data-cfasync="false"
+        async
+        src="//st.chatango.com/js/gz/emb.js"
+        style="width: 100%; height: 100%"
+      >
+        {
+            "handle": "amkailum",
+            "arch": "js",
+            "styles": {
+                "a": "f3f9e6",
+                "b": 100,
+                "c": "000000",
+                "d": "000000",
+                "k": "f3f9e6",
+                "l": "f3f9e6",
+                "m": "f3f9e6",
+                "p": "10",
+                "q": "f3f9e6",
+                "r": 100,
+                "ab": false,
+                "usricon": 0,
+                "surl": 0,
+                "allowpm": 0,
+                "cnrs": "1",
+                "fwtickm": 1
+            }
+        }
+      </script>
     </div>
 
     <br />
