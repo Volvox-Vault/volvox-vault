4650e8c70e8df03602c1079cb678f784a00a3e2d
Tiana Dueck <tianadueck@pm.me>
2024-06-13 14:08:01 -0400

    1

--- src/views/archive/2024/2/htmlday2024.vue
+++ src/views/archive/2024/2/htmlday2024.vue
@@ -31,8 +31,8 @@ Bring your own computer and note pad + pen.
 <br>
 <a href="https://lu.ma/8qp16v85?tk=WlM0Ia">RSVP here!</a>
 <br><br>
-We'll be providing an HTML cookbook, which describes the basics of HTML and CSS, as well as provides some prompts.<br>
-See you there,
+We'll be providing an HTML cookbook, which describes the basics of HTML and CSS, as well as lists some prompts.<br><br>
+See you there,<br>
 Tiana, Garry, and Ross
             </p>
 
